703c19974fd575bc968a50a1d583c3f48a7dd714
Shaun Saker <info@shaunsaker.com>
2017-04-22 08:01:29 +0200

    Build for production

--- src/reducers/initialState.js
+++ src/reducers/initialState.js
@@ -9,23 +9,10 @@ const initialState = {
         apiSuccess: null
     },
     categories: [
-        'Web App',
-        'Mobile App',
-        'Native App'        
+      
     ],
     ideas: [
-        {
-            value: 'Create a recipe app',
-            categoryId: 1
-        },
-        {
-            value: 'Create an app that takes photos of ingredients and lists any harmful additives',
-            categoryId: 1
-        },
-        {
-            value: 'Create an idea app',
-            categoryId: 0
-        },
+        
     ],
     newIdea: {
         value: null,
